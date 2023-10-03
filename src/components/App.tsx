5ab634608ef4829365b4b0a029d912a6da2f204e
Niklas <niklas_loos@web.de>
2023-10-03 14:07:33 +0200

    add routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Home from "./home/HomeSite.tsx";
 import Dish from "./dish/DishOverview.tsx";
 
 function App() {
   return (
-   <>
-    <Home />
-    <Dish />
-   </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dish" element={<Dish />} />
+      </Routes>
   )
 }
 
 export default App;
+
+
