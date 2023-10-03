04fcbe434c53292f578d2604c5633c57c3366237
Niklas <niklas_loos@web.de>
2023-10-03 23:46:35 +0200

    JSX Hamburger Menu Code hinzugefügt

--- src/components/common/Pics.tsx
+++ src/components/common/Pics.tsx
@@ -4,18 +4,19 @@ export function Logo() {
       <img 
         className="uber-eats-text-picture" 
         src="old UberEatsFiles/styles/Mainside/icons/Uber-eats-text.png" 
+        alt="Uber Eats Logo"
       />
     </a> 
   )
 }
 
 export function HamburgerMenu() {
-  return <p>Ich will Hamburger!</p>
+  return (
+    <svg className="burger-menu burger-menu-js" width="18" height="20" xmlns="http://www.w3.org/2000/svg">
+      <line x1="0" y1="4" x2="30" y2="4" stroke="black" stroke-width={2.5} />
+      <line x1="0" y1="10" x2="30" y2="10" stroke="black" stroke-width={2.5}/>
+      <line x1="0" y1="16" x2="30" y2="16" stroke="black" stroke-width={2.5}/>
+    </svg>
+  )
 }
 
-
-<svg className="burger-menu burger-menu-js" width="18" height="20" xmlns="http://www.w3.org/2000/svg">
-  <line x1="0" y1="4" x2="30" y2="4" stroke="black" stroke-width="2.5" />
-  <line x1="0" y1="10" x2="30" y2="10" stroke="black" stroke-width="2.5"/>
-  <line x1="0" y1="16" x2="30" y2="16" stroke="black" stroke-width="2.5"/>
-</svg>
