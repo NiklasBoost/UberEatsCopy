32ac31db1a0b2a4a410bfb41f3ba99ca8b477311
Niklas <niklas_loos@web.de>
2023-10-04 22:23:38 +0200

    comment out in the right way

--- src/components/dish/elements/dElements.tsx
+++ src/components/dish/elements/dElements.tsx
@@ -1,8 +1,9 @@
 export function Category() {
   return (
     <div className="category-container">
-      <img className="category-pictures" src="public/img/deals.png" /> //The right Pic must always insert here
-      <p className="category-text">Aktionen</p> // The correct text must always be inserted here
+      <img className="category-pictures" src="public/img/deals.png" /> 
+      {/* The right Pic must always insert here */}
+      <p className="category-text">Aktionen</p> {/* The correct text must always be inserted here */}
     </div> 
   )
 }
