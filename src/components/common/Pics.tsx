1b6c63eb5b49a7195a89458d29313135fe2bbc16
Niklas <niklas_loos@web.de>
2023-10-04 22:23:22 +0200

    change attribute

--- src/components/common/Pics.tsx
+++ src/components/common/Pics.tsx
@@ -13,9 +13,9 @@ export function Logo() {
 export function HamburgerMenu() {
   return (
     <svg className="burger-menu burger-menu-js" width="18" height="20" xmlns="http://www.w3.org/2000/svg">
-      <line x1="0" y1="4" x2="30" y2="4" stroke="black" stroke-width={2.5} />
-      <line x1="0" y1="10" x2="30" y2="10" stroke="black" stroke-width={2.5}/>
-      <line x1="0" y1="16" x2="30" y2="16" stroke="black" stroke-width={2.5}/>
+      <line x1="0" y1="4" x2="30" y2="4" stroke="black" strokeWidth={2.5} />
+      <line x1="0" y1="10" x2="30" y2="10" stroke="black" strokeWidth={2.5}/>
+      <line x1="0" y1="16" x2="30" y2="16" stroke="black" strokeWidth={2.5}/>
     </svg>
   )
 }
