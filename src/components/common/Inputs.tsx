0dfaef1df6f524497d61f991990d2ee07a7f66ef
Niklas <niklas_loos@web.de>
2023-10-04 22:23:10 +0200

    add jsx code

--- src/components/common/Inputs.tsx
+++ src/components/common/Inputs.tsx
@@ -1,3 +1,15 @@
 export function InputDeliveryAddress () {
-  return <input/>
+  return (
+    <>
+      <input 
+        className="
+          input-delivery-address 
+          header-input-delivery-address {/* The second class will be added, when we scroll down the site, i think */}
+        "
+        placeholder="Lieferadresse eingeben" 
+      />
+  
+    </>
+  ) 
 }
+
