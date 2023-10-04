1df6c0f7b000fef9ec1b24a64691b0c8711e5b2a
Niklas <niklas_loos@web.de>
2023-10-04 21:21:29 +0200

    add jsx code to category

--- src/components/dish/elements/dElements.tsx
+++ src/components/dish/elements/dElements.tsx
@@ -1,7 +1,13 @@
 export function Category() {
-  return <p>Das ebenso lustig wird</p>
+  return (
+    <div className="category-container">
+      <img className="category-pictures" src="public/img/deals.png" /> //The right Pic must always insert here
+      <p className="category-text">Aktionen</p> // The correct text must always be inserted here
+    </div> 
+  )
 }
 
 export function SlideshowElement() {
   return <p>Das wird noch lustig</p>
 }
+
