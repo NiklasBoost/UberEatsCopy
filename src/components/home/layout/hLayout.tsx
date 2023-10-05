1b3683cc21e563f16da1a3a4df81e671eb11a263
Niklas <niklas_loos@web.de>
2023-10-05 21:07:00 +0200

    code countries jsx code

--- src/components/home/layout/hLayout.tsx
+++ src/components/home/layout/hLayout.tsx
@@ -29,6 +29,53 @@ export function PictureOptions() {
   )
 }
 
-export function Countries(){
-  return <p>Ich Länder :)</p>
+export function Countries() {
+  return (
+    <div className="middlepart-with-countrys">
+      <div className="middlepart-with-countrys-row">
+        <div className="middlepart-with-countrys-header">
+          <h1>Länder, in denen Uber Eats verfügbar ist</h1>
+          <a href="">Alle Länder anzeigen</a>
+        </div>
+      </div>
+      <div className="middlepart-with-countrys-row">
+        <div className="country-grid-one">
+          <a>Australien</a>
+          <a>Frankreich</a>
+          <a>Mexiko</a>
+          <a>Spanien</a>
+          <a>Belgien</a>
+          <a>Großbritannien</a>
+          <a>Neuseeland</a>
+          <a>Sri Lanka</a>
+          <a>Chile</a>
+          <a>Guatemale</a>
+          <a>Niederlande</a>
+          <a>Südafrika</a>
+          <a>Costa Rica</a>
+          <a>Irland</a>
+          <a>Panama</a>
+          <a>Taiwan(VRC)</a>
+          <a>Deutschland</a>
+          <a>Italien</a>
+          <a>Polen</a>
+          <a>Vereinigten Staaten von Amerika</a>
+          <a>Dominikanische Republik</a>
+          <a>Japan</a>
+          <a>Portugal</a>
+          <a></a>
+          <a>Ecuador</a>
+          <a>Kanada</a>
+          <a>Schweden</a>
+          <a></a>
+          <a>El Salvador</a>
+          <a>Kenia</a>
+          <a>Schweiz</a>
+        </div>
+      </div>
+      <div className="middlepart-with-countrys-row">
+        <a className="middlepart-with-countrys-p">So ordnet Uber Eats die Ereignisliste. <u>Mehr zum Ranking erfahren</u></a>
+      </div>
+    </div>
+  )
 }
