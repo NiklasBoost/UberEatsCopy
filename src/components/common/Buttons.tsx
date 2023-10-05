d67a08edcb8be2c57079451891d8a3b58d342147
Niklas <niklas_loos@web.de>
2023-10-05 16:28:18 +0200

    add sign in button jsx

--- src/components/common/Buttons.tsx
+++ src/components/common/Buttons.tsx
@@ -1,5 +1,19 @@
+
+// Ich denke, es macht hier am meisten Sinn, beide JSX Strukturen drin zu haben und je nach übergebenen Props nur einen von beiden zurückzugeben 
 export function SignInButton() {
-  return <button>Ich bims, </button>
+  return (
+    <>
+      <button className="sign-in-button">
+        <div className="sign-in-button-div">
+          <img className="user-img" src="old UberEatsFiles/styles/Mainside/icons/User.png" />
+          <p className="sign-in-button-text">Anmelden</p>
+        </div>
+      </button>
+
+      <button className="login-button">Anmelden</button>
+    </>
+
+  )
 }
 
 export function RegisterButton() {
