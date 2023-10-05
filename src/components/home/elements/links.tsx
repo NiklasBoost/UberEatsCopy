ccc5a393c35488f8a3da29cffc5e2477fb7a5527
Niklas <niklas_loos@web.de>
2023-10-05 12:45:06 +0200

    wrote component and tried to put it in one

--- src/components/home/elements/links.tsx
+++ src/components/home/elements/links.tsx
@@ -9,3 +9,23 @@ export function AddRestaurant () {
 export function CourierRegistration() {
   return <p>einfach irgendwas</p>
 }
+
+
+// Die oberen drei Komponenten müsste man eigentlich zu einer fusionieren können. Im Grunde, müssen drei Informationen flexibel sein und übergeben werden: useHref, die Class und der Text --> check das mal ab, ob es geht, wenn du die Komponente darüber schreibst. Notfalls müsste man es doch wieder auf drei Komponenten aufteilen
+export function UnderSiteLinks() {
+  return (
+    <a 
+      href=""
+      className="
+        sidebar-link
+        link-to-undersite
+      "
+    >
+        Ein Geschäftskontoo anlegen; Füge dein Restaurant hinzu; Registriere dich als Kurier
+    </a>
+
+  )
+}
+
+
+
