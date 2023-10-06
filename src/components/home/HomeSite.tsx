d4f21052ebad4a23d08880f0e2bcaa515cda82b7
Niklas <niklas_loos@web.de>
2023-10-06 12:05:50 +0200

    add homesite code

--- src/components/home/HomeSite.tsx
+++ src/components/home/HomeSite.tsx
@@ -4,14 +4,15 @@ import { Footer, Sidebar } from "../common/layout/cLayout";
 
 function Home() {
   return (
-    <>
+    <div className="main-site">
+      <div className="overlay"></div>
       <HomeHeader />
       <Sidebar />
       <Delivery />
       <PictureOptions />
       <Countries />
       <Footer />
-    </>
+    </div>
   )
 }
 
