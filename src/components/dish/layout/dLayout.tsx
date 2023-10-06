5ef6b2ccbd299dadb3d813e16ec5a7878a6c2ac2
Niklas <niklas_loos@web.de>
2023-10-06 11:00:44 +0200

    put in Dish Header code

--- src/components/dish/layout/dLayout.tsx
+++ src/components/dish/layout/dLayout.tsx
@@ -4,12 +4,43 @@ import { SlideshowElement, Category } from "../elements/dElements";
 
 export function DishHeader() {
   return (
-    <>
-      <HamburgerMenu />
-      <Logo />
-      <SignInButton />
-      <RegisterButton />
-    </>
+    <div className="header">
+      <div className="header-left">
+        <HamburgerMenu />
+        <Logo />
+        <div className="deliver-collection-container">
+          <div className="white-choose-field"></div>
+          <div className="deliver-collection">
+            <div className="delivery">
+              Lieferung
+            </div>
+            <div className="collection">
+              Abholung
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="header-middle">
+        <div className="location">
+          <a></a>
+        </div>
+        <div className="input-header-container">
+            <img className="magnifier-img" src="old UberEatsFiles/styles/icons/magnifier.png" />
+            <input className="input-header" placeholder="Essen, Lebensmittel, Getränke usw." />
+        </div>
+      </div>
+      <div className="header-right">
+        <button className="header-right-shopping-cart">
+          <div className="shopping-cart-container">
+            <img className="shopping-cart-icon" src="old UberEatsFiles/styles/icons/shopping-cart-white.png" />
+            <p className="shopping-cart-text">Warenkorb &#8226; 0</p>
+          </div>  
+        </button>
+        <SignInButton />
+        <RegisterButton />
+      </div>
+    </div>
+  
   )
 }
 
