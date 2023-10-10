2c2b2ce28dc3abdf3fc46d6cb8b67ddb6bfcfec8
Niklas <niklas_loos@web.de>
2023-10-10 10:46:50 +0200

    categories randomizer

--- src/components/dish/layout/dLayout.tsx
+++ src/components/dish/layout/dLayout.tsx
@@ -1,6 +1,7 @@
 import { HamburgerMenu, Logo } from "../../common/Pics";
 import { SignInButton, RegisterButton } from "../../common/Buttons";
 import { SlideshowElement, Category } from "../elements/dElements";
+import { useEffect, useState } from "react";
 
 export function DishHeader({ setSidebarState }: {setSidebarState: React.Dispatch<React.SetStateAction<boolean>>}) {
   const signIn = 'dishHeader';
@@ -53,96 +54,91 @@ export function DishHeader({ setSidebarState }: {setSidebarState: React.Dispatch
 }
 
 export function Categories() {
-  const img = [
-    '/public/img/alcohol.png',
-    '/public/img/deals.png',
-    '/public/img/top_eats.png',
-    '/public/img/burger.png',
-    '/public/img/chinese.png',
-    '/public/img/sandwich.png',
-    '/public/img/thai.png',
-    '/public/img/sushi.png',
-    '/public/img/dessert.png',
-    '/public/img/asian.png',
-    '/public/img/american.png',
-    '/public/img/convenience.png',
-    '/public/img/pet_supplies.png',
-    '/public/img/healthy.png'
+  const categoryObjects = [
+    {
+      img: '/public/img/deals.png',
+      text: 'Angebote',
+    },
+    {
+      img: '/public/img/top_eats.png',
+      text: 'Bestes Essen',
+    },
+    {
+      img: '/public/img/alcohol.png',
+      text: 'Alkohol'
+    },
+    {
+      img: '/public/img/burger.png',
+      text: 'Burger',
+    },
+    {
+      img: '/public/img/chinese.png',
+      text: 'Chinesisch',
+    },
+    {
+      img: '/public/img/sandwich.png',
+      text: 'Sandwich',
+    },
+    {
+      img: '/public/img/thai.png',
+      text: 'Thailändisch',
+    },
+    {
+      img: '/public/img/sushi.png',
+      text: 'Sushi',
+    },
+    {
+      img: '/public/img/dessert.png',
+      text: 'Dessert',
+    },
+    {
+      img: '/public/img/asian.png',
+      text: 'Asiatisch',
+    },
+    {
+      img: '/public/img/american.png',
+      text: 'Amerikanisch',
+    },
+    {
+      img: '/public/img/convenience.png',
+      text: 'Allgemeines Zeug',
+    },
+    {
+      img: '/public/img/pet_supplies.png',
+      text: 'Tierversorgung',
+    },
+    {
+      img: '/public/img/healthy.png',
+      text: 'Gesundes',
+    }
   ];
-  const text = [
-    'Alkohol',
-    'Angebote',
-    'Bestes Essen',
-    'Burger',
-    'Chinesisch',
-    'Sandwich',
-    'Thailändisch',
-    'Sushi',
-    'Dessert',
-    'Asiatisch',
-    'Amerikanisch',
-    'Allgemeines Zeug',
-    'Tierversorgung',
-    'Gesundes'
-  ];
+  const [categories, setCategories] = useState<{ img: string; text: string; }[]>([]);
+
+  //category randomizer
+  useEffect(() => {
+    const remainingCategories = [...categoryObjects.slice(2)];
+    for (let i = remainingCategories.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [remainingCategories[i], remainingCategories[j]] = [remainingCategories[j], remainingCategories[i]];
+    }
+
+    const firstTwoCategories = categoryObjects.slice(0, 2);
+
+    const displayCategories = [...firstTwoCategories, ...remainingCategories];
+
+    setCategories(displayCategories);
+  }, []);
+
+
   return (
     <div className="categorys-container">
-      <Category 
-        categoryImg={img[0]}
-        categoryText={text[0]}
-      />
-       <Category 
-        categoryImg={img[1]}
-        categoryText={text[1]}
-      />
-      <Category 
-        categoryImg={img[2]}
-        categoryText={text[2]}
-      />
-       <Category 
-        categoryImg={img[3]}
-        categoryText={text[3]}
-      />
-      <Category 
-        categoryImg={img[4]}
-        categoryText={text[4]}
-      />
-       <Category 
-        categoryImg={img[5]}
-        categoryText={text[5]}
-      />
-      <Category 
-        categoryImg={img[6]}
-        categoryText={text[6]}
-      />
-       <Category 
-        categoryImg={img[7]}
-        categoryText={text[7]}
-      />
-      <Category 
-        categoryImg={img[8]}
-        categoryText={text[8]}
-      />
-      <Category 
-        categoryImg={img[9]}
-        categoryText={text[9]}
-      />
-      <Category 
-        categoryImg={img[10]}
-        categoryText={text[10]}
-      />
-      <Category 
-        categoryImg={img[11]}
-        categoryText={text[11]}
-      />
-      <Category 
-        categoryImg={img[12]}
-        categoryText={text[12]}
-      />
-      <Category 
-        categoryImg={img[13]}
-        categoryText={text[13]}
-      />
+      {categories.map((category, index) => (
+        <Category 
+          key={index}
+          categoryImg={category.img}
+          categoryText={category.text}
+        />
+      ))}
     </div>
   )
 }
